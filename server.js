e258d2ecf16fbb8e4ef4fe60ef42b0c7d2271be7
adrean99 <adreankiai@gmail.com>
2025-04-03 09:17:43 +0300

    Fully remove Socket.IO from server.js and package.json

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const http = require("http");
-const { Server } = require("socket.io");
 const path = require("path");
 
 const app = express();
